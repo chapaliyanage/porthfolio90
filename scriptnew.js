fed853aa36baf439959a6cd6a3075f359dfec175
chapaliyanage <99138315+chapaliyanage@users.noreply.github.com>
2023-06-10 17:33:07 +0530

    Add files via upload

---
+++ scriptnew.js
@@ -0,0 +1,25 @@
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+
+
+
+
+
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
